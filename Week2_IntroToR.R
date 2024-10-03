f6f926684b4693473327a223b7c00b7a203e8523
BROCK RIGGS <bmriggs@svm.ucdavis.edu>
2024-10-03 14:29:52 -0700

    In-class work week 2

--- Week2_IntroToR.R
+++ Week2_IntroToR.R
@@ -6,3 +6,10 @@ elephant1_lb <- elephant1_kg * 2.2
 myelephants <- c(elephant1_lb, elephant2_lb)
 
 which(myelephants == max(myelephants))
+
+getwd()
+"C:/Users/bmriggs/Documents/R-Projects/R_DAVIS_in_class_project_bmriggs"
+
+dir.create("./lectures")
+dir.create("./homework")
+
