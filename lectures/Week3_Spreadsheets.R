060ff07c51a71292d9e10160f93c55bcf03dcfdf
BROCK RIGGS <bmriggs@svm.ucdavis.edu>
2024-10-10 14:51:47 -0700

    In-Class notes Week 3 Spreadsheets

---
+++ lectures/Week3_Spreadsheets.R
@@ -0,0 +1,43 @@
+surveys <- read.csv("data/portal_data_joined.csv")
+nrow(surveys)
+ncol(surveys)
+class(surveys)
+str(surveys)
+surveys
+
+summary(surveys)
+
+surveys[1, 5] #specific location in table
+1:5 #first 5 rows
+surveys[1:5,] #first 5 rows, all columns
+
+surveys[,1] #all rows, first column
+dim(surveys[1]) #just columns, need comma to specify rows
+
+colnames(surveys)
+
+surveys[c('record_id', 'year', 'day')]
+dim(surveys[c('record_id', 'year', 'day')])
+
+head(surveys) #visualize head of table
+head(surveys, n=25) #visulaize head of table to desired number of rows
+
+tail(surveys) #shows bottom (tail) of table
+
+head(surveys["genus"])
+head(surveys[["genus"]])
+
+head(surveys['genus',]) #treated as a row
+head(surveys[,'genus'])
+
+surveys$genus
+
+surveys$hindfoot_length
+class(surveys$hindfoot_length)
+
+
+
+
+
+
+
